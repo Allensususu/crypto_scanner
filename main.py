54fb6642231793c9f64f4047a5a68ea9a1c3b404
Allen <allen132allen@gmail.com>
2022-07-04 22:02:40 +0800

    change main.py

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ import finlab_crypto
 import time 
 from finlab_crypto import Strategy
 import datetime
-
+from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 api_key = "J0gq06B1YlsLVPfkkJ3LAEPHMOwQ74EaUGXiDBEyCiGIkRuDJAZz95RThMFmgyyc"
 api_secret = "9L828724dXaEB6z0V91b5GoodFdmP93qMe9vExUHI4eTv4dx0ozWCl4wSrvavId9"
 finlab_crypto.setup()
-from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+
 
 client = Client(api_key, api_secret)
 ticker = []
 
-total_currency = 0
+total_20MA = 0
 above_20MA = 0
+total_50MA = 0
 above_50MA = 0
+total_200MA = 0
 above_200MA = 0
 
 def list_get_usdt_ticker():
@@ -33,6 +35,9 @@ def list_get_usdt_ticker():
                "USDCUSDT" not in p["symbol"] ):
                 ticker.append(p["symbol"])
 
+
+
+
 list_get_usdt_ticker()
 for p in ticker:
     ohlcv = finlab_crypto.crawler.get_all_binance(p, '1d')
@@ -43,35 +48,24 @@ for p in ticker:
         #pass
 
     #total_currency += 1
- 
-    """
-    if (len(ohlcv)) > 200 :
+    if len(ohlcv) > 20 :
+        total_20MA += 1 
         if close[-1] > close.rolling(20).mean()[-1]:
             above_20MA += 1
+    if len(ohlcv) > 50 :
+        total_50MA += 1 
         if close[-1] > close.rolling(50).mean()[-1]:
             above_50MA += 1
+    if len(ohlcv) > 2000 :
+        total_200MA += 1 
         if close[-1] > close.rolling(200).mean()[-1]:
             above_200MA += 1
-    else if:
-        if close[-1] > close.rolling(10).mean()[-1]:
-            above_20MA += 1
-        if close[-1] > close.rolling(int(len(ohlcv)/3)).mean()[-1]:
-            above_50MA += 1
-        if close[-1] > close.rolling(int(len(ohlcv)/3*2)).mean()[-1]:
-            above_200MA += 1
-            """
-    if close[-1] > close.rolling(20).mean()[-1]:
-        above_20MA += 1
-    if close[-1] > close.rolling(50).mean()[-1]:
-        above_50MA += 1
-    if close[-1] > close.rolling(200).mean()[-1]:
-        above_200MA += 1
         
 
 total_currency = 0
 print(total_currency)
-print(above_20MA)
-print(above_50MA)
-print(above_200MA)
+print(above_20MA / total_20MA)
+print(above_50MA / total_50MA)
+print(above_200MA / total_200MA)
 
 #print(get_all_history("DOGEUSDT"))
